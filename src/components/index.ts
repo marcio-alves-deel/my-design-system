006119254ed4ef3ff502f3640e02bd81ef386354
Marcio Alves <marcio.alves@letsdeel.com>
2024-05-13 10:47:12 -0300

    feat: Updating components and stories

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export { Button } from "./Button";
 export { List } from "./List";
 export { Select } from "./Select";
 export { RadioGroup } from "./RadioGroup";
+export { Alert } from "./Alert";
+export { Chip } from "./Chip";
