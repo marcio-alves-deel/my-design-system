2907194f22933fa70a62a566ee66b5288b1e241d
Marcio Alves <marcio.alves@letsdeel.com>
2024-05-13 00:13:48 -0300

    feat: adding initial config + details

---
+++ dist/providers/ThemeProvider/ThemeProvider.d.ts
@@ -0,0 +1,5 @@
+import { FunctionComponent, PropsWithChildren } from "react";
+interface ThemeProviderProps extends PropsWithChildren {
+}
+declare const ThemeProvider: FunctionComponent<ThemeProviderProps>;
+export default ThemeProvider;
---
+++ dist/providers/index.d.ts
@@ -0,0 +1,1 @@
+export { DefaultThemeProvider } from "./ThemeProvider";
